c09fd4f63bf8fc592bbd4259f5a49ec779d786bd
PebblerWon <pebblerwon@gmail.com>
2018-02-24 16:27:11 +0800

    change port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.get('/',function(req,res){
 	res.send('Hello World')
 })
 
-var server = app.listen(3000,function(){
+var server = app.listen(80,function(){
 	var host = server.address().address
 	var port = server.address().port
 	console.log('Example app listening at htth://%s:%s',host,port)
