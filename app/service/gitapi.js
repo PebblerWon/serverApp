0fa4f5f24dc28d30b48ee447136b8637e432a519
pebblerwon <pebblerwon@gmail.com>
2018-03-21 09:42:51 +0800

    no

--- app/service/gitapi.js
+++ app/service/gitapi.js
@@ -1,5 +1,4 @@
 const Service = require('egg').Service
-<<<<<<< HEAD
 const Config = require('../../config/config.default.js')
 
 class GitapiService extends Service{
@@ -21,15 +20,6 @@ class GitapiService extends Service{
 				Authorization: `token ${gitToken}`
 			},
 			dataType:'json'
-=======
-const cheerio = require('cheerio')
-
-class GitapiService extends Service{
-	async test(){
-		const {serverUrl} = this.config.gitapi
-		const result = await this.ctx.curl(`${serverUrl}?q=stars%3A>1000&type=`,{
-			dataType:'text'
->>>>>>> 253f930021203b651244ac7a0f7b8f76a8840e76
 		});
 		const $ = cheerio.load(result.data)
 		let a = $('ul.repo-list>div.repo-list-item>div>h3>a')['0']
