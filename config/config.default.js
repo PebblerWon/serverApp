bd47eb0153e68d2ce4fc81249b9575060fb5ee7d
pebblerwon <pebblerwon@gmail.com>
2018-04-10 16:11:33 +0800

    '2018/04/10'

--- config/config.default.js
+++ config/config.default.js
@@ -28,7 +28,7 @@ module.exports = appInfo => {
   config.gitapi={
     serverUrl:'https://api.github.com',
     serverUrl2:'https://developer.github.com/v3/',
-    gitToken:'e11589e16b6a884080aea327276337f4390612c6'
+    gitToken:'dfac227af526e8___666a438ac7982a3c8b7938cf53'
   }
   return config;
 };
